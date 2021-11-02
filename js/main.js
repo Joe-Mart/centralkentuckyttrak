fc46f533683678c21b53c5220c31fd1522d15096
Joe Martin <joemartinky@gmail.com>
2021-11-01 20:50:06 -0400

    form validation js.  Started on js code to send data to local storage,

--- js/main.js
+++ js/main.js
@@ -2,7 +2,50 @@
 "use strict";
 
 function validateForm() {
-    alert("it worked")
+    var status = true;
+    var x;
 
+    //test for first name
+    x = document.forms.myForm.fname.value;
+    if (x===null || x==="") {
+        status=false;
+        document.getElementById('fname').className = "error"
+    }
 
+    // test for last name
+    x = document.forms.myForm.lname.value;
+    if (x===null || x==="") {
+        status=false;
+        document.getElementById('lname').className = "error"
+    }
+
+    // test for email
+    x = document.forms.myForm.email.value;
+    if (x===null || x==="") {
+        status=false;
+        document.getElementById('email').className = "error"
+    }
+
+    // test for comment
+    x = document.forms.myForm.comment.value;
+    if (x===null || x==="") {
+        status=false;
+        document.getElementById('comment').className = "error"
+    }
+
+    return status;
 } // end of the form
+
+//stores items in the localStorage
+
+/*function store(){ 
+var fname = document.getElementById('fname').value;
+var lname = document.getElementById('lastname').value;
+var email = document.getElementById('email').value;
+var comment = document.getElementById('comment').value;
+
+}
+
+window.localStorage.setItem(fname,lname,email,comment);  
+
+}*/
